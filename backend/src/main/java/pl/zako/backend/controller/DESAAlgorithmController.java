94aa75bedd77bcbf37c5e795cd82e8d7299f41ba
Kamil Korenkiewicz <kamil@Kamils-MacBook-Air.local>
2022-05-10 07:15:56 +0200

    DES algoritm controller done

--- backend/src/main/java/pl/zako/backend/controller/DESAAlgorithmController.java
+++ backend/src/main/java/pl/zako/backend/controller/DESAAlgorithmController.java
@@ -1,19 +1,28 @@
 package pl.zako.backend.controller;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.zako.backend.DTO.DesDto;
+import pl.zako.backend.DTO.GeneratorBskDto;
 
 @RestController
 public class DESAAlgorithmController {
 
-    @PostMapping("/DES/encode")
-    public String getEncodingResultFromDESAlgorythm(){
-        return null;
+    @GetMapping("/DES/key")
+    public String getRandom_64_bitsKey(){
+        GeneratorBskDto aGeneratorBskDto = new GeneratorBskDto(64);
+        return GeneratorBskDto.codeAndReturnString(aGeneratorBskDto);
     }
 
-    @PostMapping("/DES/decode")
-    public String getDecodingResultFromDESAlgorythm(){
-        return null;
+    @GetMapping("/DES/encode")
+    public String encode(@RequestBody String aMessage, String aKey){
+        return DesDto.encode(aMessage, aKey);
+    }
+
+    @GetMapping("/DES/decode")
+    public String decode(@RequestBody String aBitsToDecode, String aKey){
+        return DesDto.decode(aBitsToDecode, aKey);
     }
 }
